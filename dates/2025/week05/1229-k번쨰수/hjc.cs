5dd52b92c83b47c56f5d406975f8917ceed9710a
HuuiJi <120005259+yoreleihee@users.noreply.github.com>
2025-12-29 19:02:46 +0900

    1229-k번쨰수 풀이

---
+++ dates/2025/week05/1229-k번쨰수/hjc.cs
@@ -0,0 +1,27 @@
+﻿public class Program
+{
+    static int[] solution(int[] array, int[,] commands)
+    {
+        int   commandCount = commands.GetLength(0);
+        int[] answer       = new int[commandCount];
+        
+        for (int idx = 0; idx < commandCount; idx++)
+        {
+            int i = commands[idx, 0] - 1;
+            int j = commands[idx, 1];
+            int k = commands[idx, 2] - 1;
+
+            int[] newArray = new int[j - i];
+            for (int l = 0; l < newArray.Length; l++)
+            {
+                newArray[l] = array[i + l];
+            }
+            
+            Array.Sort(newArray);
+
+            answer[idx] = newArray[k];
+        }
+        
+        return answer;
+    }
+}
